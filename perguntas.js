ab3a4aab14015e94b39221c0b4bcbea7bd3f4d91
theuscompany <168585450+theuscompany@users.noreply.github.com>
2024-11-05 15:26:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quem foi responsável pela unificação da Alemanha em !871?',
+    'Otto von Bismarck'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país tem a maior extensão territorial do mundo?',
+    'A RÚSSIA'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o número primo que, quando multiplicado pelo próximo primo maior que ele,resulta em 143?',
+    '11'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
